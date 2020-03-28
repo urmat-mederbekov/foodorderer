744a94c6cdf6951bef6a19fdfdd0db09d48d1245
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-28 16:38:07 +0600

    for testing

---
+++ src/test/java/kg/attractor/foodorderer/FoodordererApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.foodorderer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodordererApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
