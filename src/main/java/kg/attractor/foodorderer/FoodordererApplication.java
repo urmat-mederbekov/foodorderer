5e862aa7afa2eb1b352bf3d46857b61ba246f388
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-03-28 16:40:16 +0600

    file added for launching a project

---
+++ src/main/java/kg/attractor/foodorderer/FoodordererApplication.java
@@ -0,0 +1,13 @@
+package kg.attractor.foodorderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodordererApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodordererApplication.class, args);
+    }
+
+}
